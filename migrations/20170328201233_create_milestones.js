5ceceb10fd9839952560bf9e436f9ff42590efc8
Raven Ackerman <rayvayvo@gmail.com>
2017-03-28 20:52:30 +0000

    created a new migrate file and updated

--- migrations/20170328201233_create_milestones.js
+++ migrations/20170328201233_create_milestones.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('milestones', function(table){
-      table.increments('id');
-      table.string('description');
-      table.date('date_achieved');
-    })
-  ])
+  // return Promise.all([
+  //   knex.schema.createTable('milestones', function(table){
+  //     table.increments('id');
+  //     table.string('description');
+  //     table.date('date_achieved');
+  //   })
+  // ])
 };
 
 exports.down = function(knex, Promise) {
